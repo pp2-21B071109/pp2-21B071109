62ef4566475549d86572a16d3d982b5ec70b20ed
pp2_21B071109 <d_ismailov@kbtu.kz>
2023-03-09 02:54:41 +0600

    tsis6

--- tsis6/built.py
+++ tsis6/built.py
@@ -26,6 +26,7 @@ s = input()
 func(s)
 """
 #3)Write a Python program with builtin function that checks whether a passed string is palindrome or not.
+"""
 def is_palindrome(word):
     word = word.lower()
     reversed_word = ''.join(reversed(word))
@@ -35,3 +36,26 @@ def is_palindrome(word):
         return False
 word = input()
 print(is_palindrome(word))
+"""
+
+#4)Write a Python program that invoke square root function after specific milliseconds.
+"""
+import math
+import time
+def sqrt_after_ms(num, ms):
+    time.sleep(ms/1000)
+    return math.sqrt(num)
+
+num = int(input())
+ms = int(input())
+result = sqrt_after_ms(num, ms)
+print(f"Square root of {num} after {ms} milliseconds is {result}")
+"""
+#5)Write a Python program with builtin function that returns True if all elements of the tuple are true.
+def all_true(t):
+    return all(t)
+
+t1 = (True, True, True)
+t2 = (True, False, True)
+print(all_true(t1))
+print(all_true(t2))
